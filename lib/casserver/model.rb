705b11f56a818243b659791568eb85451db4c01f
Frederik Fix <ich@derfred.com>
2012-08-14 09:38:26 +0200

    this should finally fix it

--- lib/casserver/model.rb
+++ lib/casserver/model.rb
@@ -69,7 +69,7 @@ module CASServer::Model
 
     def clean_service_url(service_url)
       url = URI.parse(service_url)
-      URI.join(url.scheme, url.host, url.port).to_s
+      "#{url.scheme}://#{url.host}:#{url.port}"
     end
 
     def matches_service?(service)
